dbf3540cfe8a1cff6ad6c35968014b136bdf951a
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-03-10 21:20:14 -0300

    Botões
    Criei uma tela "home" com três botões.

---
+++ src/Telas/inicial.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import 'inicial.css';
+
+function home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button>Botão 1</button>
+        <button>Botão 2</button>
+        <button>Botão 3</button>
+      </header>
+    </div>
+  );
+}
+
+export default home;
---
+++ src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import Inicial from './Telas/inicial';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Inicial />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your Home, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
