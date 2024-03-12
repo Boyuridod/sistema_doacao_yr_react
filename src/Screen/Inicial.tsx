65aad1681630fc80df2327e7f03cd114f37f5b7a
Yuri David Silva Duarte <102871804+Boyuridod@users.noreply.github.com>
2024-03-12 13:37:19 -0300

    Fazendo em react de verdade
    Refazendo os botões mas usando componentes react

---
+++ src/Screen/Inicial.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MyButton from '../Components/Button';
+// import 'inicial.css';
+
+function Inicial() {
+  return (
+    <div>
+      <MyButton />
+    </div>
+  );
+}
+
+export default Inicial;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Inicial from './Screen/Inicial';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Inicial />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
