c4717a9357e380beb7238bfce30675946bc8ea1c
Évora <122195163+Rafaelevora@users.noreply.github.com>
2024-03-12 20:26:02 -0300

    Commit

---
+++ projeto/src/index.tsx
@@ -0,0 +1,12 @@
+// src/index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
