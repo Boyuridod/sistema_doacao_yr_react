4021670738c935cc4ac1dade409ce3ace17949c6
Évora <122195163+Rafaelevora@users.noreply.github.com>
2024-03-21 06:45:27 -0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import Inicial from './Screen/Inicial/Inicial.tsx';
 import Formulario from './Screen/Formulario/Formulario.tsx';
-// import NewFormulario from './Screen/Formulario/NewFormulario.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Inicial /> */}
     <Formulario />
-    {/* <NewFormulario /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
